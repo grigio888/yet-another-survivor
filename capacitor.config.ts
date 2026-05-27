48f93197b8223ad019d8ae2a7b40c2bf6a4ed5c5
grigio888 <vinicius_grigio@hotmail.com>
2026-05-27 23:38:54 +0100

    feat: added capacitor as multiplatform framework.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.another_test',
+  appName: 'another-test',
+  webDir: 'build'
+};
+
+export default config;
